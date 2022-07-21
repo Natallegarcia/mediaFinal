075ab73b897332ead0c7c44cc6a44bb0f7da47c3
Nátalle Garcia <natallegarcia2015@gmail.com>
2022-07-21 20:06:26 -0300

    first commit

---
+++ README.md.md
@@ -0,0 +1,2 @@
+Algorítmo que calcula a média final e retorna para o aluno o resultado de aprovação ou reprovação. Utilizei a Estrutura Condicional IF/ELSE, e operações matemáticas. 
+s
---
+++ mediaFinal.js
@@ -0,0 +1,20 @@
+
+let aluno = 'Rafaela';
+let nota1 = 6;
+let nota2= 6.5;
+let nota3 = 7;
+let mediaFinal = (nota1 + nota2 + nota3) / 3; 
+
+
+
+console.log("Bem vinda  " + aluno);
+
+console.log ("Sua nota final é  " + mediaFinal);
+
+if (mediaFinal >= 6) {
+ console.log ("Parabéns, você foi aprovada na diciplina !!");
+} 
+else { 
+console.log ("Você não foi aprovado nessa diciplina   " + aluno);
+}
+
